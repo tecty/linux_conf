b811351f9f045365a8a8467f39e97dc49b4c8857
Toby Huang <619058900@qq.com>
2017-10-01 03:42:16 +1100

    some script on install themes.

--- apply.sh
+++ apply.sh
@@ -3,4 +3,8 @@
 sudo apt update && sudo apt upgrade
 sudo apt install zsh i3 feh rofi compton i3blocks tmux
 
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install space vim
+curl -sLf https://spacevim.org/install.sh | bash
