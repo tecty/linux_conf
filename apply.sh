558d62989fdec02c1030664160be40ef1003409f
Toby Huang <619058900@qq.com>
2017-10-01 03:39:43 +1100

    try to include tmux settings

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt update && sudo apt upgrade
-sudo apt install zsh i3 feh rofi compton i3blocks
+sudo apt install zsh i3 feh rofi compton i3blocks tmux
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
