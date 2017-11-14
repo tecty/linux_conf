5d3302ba643f0fcf9da1bb0f26c04928431aa71e
Toby Huang <619058900@qq.com>
2017-11-14 03:31:48 -0800

    add some basic config files, modify some shell scripts

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt update && sudo apt upgrade
-sudo apt install zsh i3 feh rofi compton i3blocks tmux
+sudo apt install zsh tmux vim htop clang python3 git -y
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -9,9 +9,17 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # install space vim
 curl -sLf https://spacevim.org/install.sh | bash
 
+# install the fonts
+sh depend_sh/font_install.sh
 
-# copy the i3 manager's config
-cp i3/ ~/.config/
+# copy the config files
+sh depend_sh/cp_conf.sh
 
-# give all the script to run primission
-chmod +x ~/.config/i3/desktop.sh ~/.config/i3/i3exit.sh 
+
+
+
+## copy the i3 manager's config
+# cp i3/ ~/.config/
+#
+# # give all the script to run primission
+# chmod +x ~/.config/i3/desktop.sh ~/.config/i3/i3exit.sh
